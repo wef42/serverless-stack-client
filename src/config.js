c3788e8eec186f1efd50a17f9f4ad25ff5ecf629
unknown <warwick.freeman@gmail.com>
2019-10-26 10:31:50 +1100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "US East (N. Virginia)",
+    BUCKET: "notes-app-uploads-wef"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://98wik1pha4.execute-api.us-east-1.amazonaws.com"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_pn28LAQMp",
+    APP_CLIENT_ID: "5nrbrp4vc07id8oes3kph7qeau",
+    IDENTITY_POOL_ID: "us-east-1:2a70d668-c613-48ac-a423-18e1b19de80b"
+  }
+};
